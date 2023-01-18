8bf5b545e4ef886ed2992c6d5d27285cd503f845
Ricardo <106403365+roicoroyAmigao@users.noreply.github.com>
2023-01-18 18:18:06 +0000

    updated with!

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Web Github',
+      title: 'Flutter Web Github!',
       theme: ThemeData(
         // This is the theme of your application.
         //
